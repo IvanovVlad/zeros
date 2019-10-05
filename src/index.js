9b0b2989a628a0bc44ee63cc3745698d7f4b099c
Vlad <iwanow.wld@gmail.com>
2019-10-05 12:21:00 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,64 +1,64 @@
-function coefFact(num, coef) {
-  if (num <= 3) {
-    return num
-  }
-  else {
-    if (coef == 1 || (coef == 2 && num % 2 != 0)) {
-      let tmp = 1;
-      for (let i = 1 + coef; i <= num; i += coef)
-        tmp = tmp * i;
-      return tmp;
-    }
-    else {
-      let tmp = 2;
-      for (let i = 4; i <= num; i += coef) {
-        tmp = tmp * i;
-      }
-      return tmp;
-    }
-  }
-
+let nol = BigInt(0);
+let odin = BigInt(1);
+let dva = BigInt(2);
+let n;
+function factorial(num) {
+  n = BigInt(num);
+   if (n == nol)
+      { return odin; }
+    else
+      { return n * factorial( n - odin ); }
 }
-function calculate(expr) {
-  let result = BigInt(1);
-    let substring = expr;
-    if (expr.includes('*')) {
-      substring = expr.split('*')
-      for (let i = 0; i < substring.length; i++) {
-        if (substring[i].includes('!!')) {
-          substring[i] = substring[i].replace('!!', '');
-          result *= BigInt(coefFact(Number(substring[i]), 2));
-        }
-        else {
-          substring[i] = substring[i].replace('!', '');
-          result *= BigInt(coefFact(Number(substring[i]), 1));
-        }
-      }
+function factorial2(nat) {
+  let n = BigInt(nat);
+    if (n == nol || n == odin){
+        return odin; 
     }
-    else{
-      if (substring.includes('!!')) {
-          substring = substring.replace('!!', '');
-          result *= BigInt(coefFact(Number(substring), 2));
-        }
-        else {
-          substring = substring.replace('!', '');
-          result *= BigInt(coefFact(Number(substring), 1));
-        }
-    }
-    return result;
+    return n * factorial2(n - dva); 
 }
 
 module.exports = function zeros(expression) {
-  let result = String(calculate(expression));
-  console.log(result);
+	console.log(expression)
+  let arrayOfFactorials;
+  let result = BigInt(1);
+  if (expression.includes('*')){
+    arrayOfFactorials = expression.split('*');
+    for (let i = 0; i < arrayOfFactorials.length; i++)
+    {
+      if (arrayOfFactorials[i].includes('!!'))
+      {
+        arrayOfFactorials[i] = arrayOfFactorials[i].replace('!!', '');
+        arrayOfFactorials[i] = factorial2(arrayOfFactorials[i]);
+        result *= arrayOfFactorials[i];
+      }
+      else{
+        arrayOfFactorials[i] = arrayOfFactorials[i].replace('!', '');
+        arrayOfFactorials[i] = factorial(arrayOfFactorials[i]);
+        result *= arrayOfFactorials[i];
+      }
+    }
+  }
+  else{
+    arrayOfFactorials = expression;
+    if (arrayOfFactorials.includes('!!'))
+      {
+        arrayOfFactorials = arrayOfFactorials.replace('!!', '');
+        arrayOfFactorials = factorial2(arrayOfFactorials);
+        result = arrayOfFactorials;
+      }
+      else{
+        arrayOfFactorials = arrayOfFactorials.replace('!', '');
+        arrayOfFactorials = factorial(arrayOfFactorials);
+        result = arrayOfFactorials;
+      }
+  }
+  result = String(result);
   let counter = 0;
   let i = result.length-1;
-  console.log(result[i])
   while(result[i] === '0')
   {
     counter++;
     i--;
   }
-  
   return counter;
 }
